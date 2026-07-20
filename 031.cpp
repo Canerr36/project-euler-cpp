7286c5a97be7e8b91d45e295ae5fb10f27740e04
Canerr36 <141125860+Canerr36@users.noreply.github.com>
2026-07-21 01:50:10 +0300

    Add files via upload

---
+++ 031.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+
+
+int main()
+{
+   
+    
+    int coin[8] = { 1,2,5,10,20,50,100,200 };
+    long long dp[201] = {0};
+
+    dp[0] = 1;
+
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = coin[i]; j <= 200; j++)
+        {
+            dp[j] += dp[j - coin[i]];
+        }
+    }
+
+    std::cout << dp[200];
+
+    return 0;
+
+
+
+}
