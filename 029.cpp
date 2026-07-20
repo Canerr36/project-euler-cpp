1baac818ee43cbb9a052e784894ec2d504a8bf0e
Canerr36 <141125860+Canerr36@users.noreply.github.com>
2026-07-21 01:08:45 +0300

    Add files via upload

---
+++ 029.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <math.h>
+#include <set>
+
+
+int main()
+{
+    std::set<double> s;
+
+
+    for (int i = 2; i <= 100; i++) {
+        for (int j = 2; j <= 100; j++) {
+
+           // std::cout<<i<<"^"<<j<<" :  " << pow(i, j) << std::endl;
+            s.insert(pow(i, j));
+        }
+    }
+    std::cout << s.size();
+
+
+}
+
